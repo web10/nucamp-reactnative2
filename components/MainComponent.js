9399e62f09283d57a6ac2f069ee0cf67e31ba8ba
web10 <danh1975@yahoo.com>
2020-11-22 11:06:29 -0800

    onpress not working yet

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
+import { View } from 'react-native';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            selectedCampsite: ''
         };
     }
 
+    onCampsiteSelect(campsiteID){
+        this.setState({selectedCampsite: campsiteID});
+    }
+
     render() {
-        return <Directory campsites={this.state.campsites} />;
+        return (
+            <View style={{flex: 1}}>
+                <Directory 
+                    campsites={this.state.campsites} 
+                    onPress={campsiteID => this.onCampsiteSelect(campsiteId)}
+                />
+                <CampsiteInfo
+                    campsite={this.state.campsites.filter(
+                        campsite => campsite.id === this.state.selectedCampsite[0])}
+                />
+           </View>
+        )  
     }
 }
 
-export default Main;
+export default Main
