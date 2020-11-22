297bbf0d2fe46c9570ea1660d5e8b0a81dde33aa
web10 <danh1975@yahoo.com>
2020-11-22 10:06:54 -0800

    changed App component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Main from './components/MainComponent';
-import { StyleSheet, Text, View } from 'react-native';
+ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
